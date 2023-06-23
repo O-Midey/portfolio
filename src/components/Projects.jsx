635abab4f6fa35b21723b6cf2e797a2cb125d49b
Mide <talk2adeoluwa2310@gmail.com>
2023-06-23 10:36:40 +0100

    fixed can't resolve #2

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import React from "react";
-import ProjectTile from "./projectTile";
+import ProjectTile from "C:UsersodetoDesktopportfoliosrccomponentsProjectTile.jsx";
 
 export const Projects = () => {
   return (
